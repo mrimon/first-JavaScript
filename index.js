a044c80df96a35efeea34ff5a68834c04973038d
mrimon <rmamun1585@gmail.com>
2025-02-05 08:50:58 +0600

    calculating fahrenheit to celsius

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
-/** task 3 from module 1 - milestone 4**/ 
-// You task is to divide the given number by 5 and show the remainder as the output.
+// how to calculate fahrenheit to celsius  
 
-var givenNum = 773;
-// remainder will be 
-var rem = givenNum % 5;
-document.write(rem);
+var fah = parseFloat(prompt("Enter Fahrenheit :"));
+var cel = ((fah -32) * (5/9).toFixed(2));
+
+document.write("Celsius =" + cel);
